f0248eca04fd464aea79d518e3e5f95eb2cd4d36
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-07-18 18:02:07 +0200

    improvement wordle

--- src/pages/wordle/components/Row.jsx
+++ src/pages/wordle/components/Row.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
+  // Previous guesses
   if (guess) {
     return (
       <div className="gridRow past">
@@ -8,6 +9,24 @@ export default function Row({ guess }) {
           <div key={i} className={l.color}>
             {l.key}
           </div>
+        ))}
+      </div>
+    );
+  }
+
+  if (currentGuess) {
+    // split string into an array
+    let letters = currentGuess.split("");
+
+    return (
+      <div className="gridRow current">
+        {letters.map((letter, i) => (
+          <div key={i} className="filled">
+            {letter}
+          </div>
+        ))}
+        {[...Array(5 - letters.length)].map((_, i) => (
+          <div key={i}></div>
         ))}
       </div>
     );

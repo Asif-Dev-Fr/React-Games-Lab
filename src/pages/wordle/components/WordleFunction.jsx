f57f381395ce29e6152fb13a7cca66e6f474c3fb
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-07-21 18:22:56 +0200

    wordle game end done

--- src/pages/wordle/components/WordleFunction.jsx
+++ src/pages/wordle/components/WordleFunction.jsx
@@ -1,22 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import useWordle from "../../../hooks/useWordle";
 import Grid from "./Grid";
 import Keypad from "./Keypad";
+import Modal from "./Modal";
 
 export default function WordleFunction({ solution }) {
   const { currentGuess, handleKeyup, guesses, isCorrect, turn, usedKeys } =
     useWordle(solution);
-  console.log(currentGuess);
+  const [showModal, setShowModal] = useState(false);
+  const [ignored, forceUpdate] = useReducer((x) => x + 1, 0);
 
   useEffect(() => {
     window.addEventListener("keyup", handleKeyup);
 
+    if (isCorrect) {
+      setTimeout(() => setShowModal(true), 2000);
+      console.log(showModal);
+      window.removeEventListener("keyup", handleKeyup);
+    } else if (turn > 5) {
+      setTimeout(() => setShowModal(true), 2000);
+      console.log(showModal);
+      window.removeEventListener("keyup", handleKeyup);
+    }
+
     return () => window.removeEventListener("keyup", handleKeyup);
-  }, [handleKeyup]);
+  }, [handleKeyup, isCorrect, turn]);
 
   useEffect(() => {
     console.log(guesses, turn, isCorrect);
   }, [guesses, turn, isCorrect]);
+
+  const closeSideMenu = () => {
+    if (showModal) {
+      setShowModal(false);
+      forceUpdate();
+    }
+  };
 
   return (
     <div>
@@ -24,6 +43,14 @@ export default function WordleFunction({ solution }) {
       <div>Current Guess - {currentGuess}</div>
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
       <Keypad usedKeys={usedKeys} />
+      {showModal && (
+        <Modal
+          isCorrect={isCorrect}
+          turn={turn}
+          solution={solution}
+          closeSideMenu={closeSideMenu}
+        />
+      )}
     </div>
   );
 }

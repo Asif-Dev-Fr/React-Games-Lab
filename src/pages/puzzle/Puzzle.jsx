f282d7425ebc4af675dec4418289774ae30f0e57
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-08-14 12:49:50 +0200

    add difficulty

--- src/pages/puzzle/Puzzle.jsx
+++ src/pages/puzzle/Puzzle.jsx
@@ -8,6 +8,7 @@ import Default from "../../assets/images/puzzleJapon.jpg";
 const Puzzle = () => {
   const [showInput, setShowInput] = useState(false);
   const [image, setImage] = useState(null);
+  const [difficulty, setDifficulty] = useState(null);
 
   // Methods
   const chooseImage = (selection) => {
@@ -20,37 +21,78 @@ const Puzzle = () => {
     }
   };
 
+  const chooseDifficulty = (number) => {
+    setImage(null);
+    setShowInput(false);
+
+    switch (number) {
+      case "easy":
+        setDifficulty(3);
+        break;
+
+      case "intermediate":
+        setDifficulty(4);
+        break;
+
+      case "hard":
+        setDifficulty(5);
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="puzzleContainer">
       <h2>Puzzle</h2>
-      <div className="chooseImage">
-        <Button variant="warning" onClick={() => chooseImage("default")}>
-          Default Image
+
+      <div className="chooseDifficulty">
+        <Button variant="warning" onClick={() => chooseDifficulty("easy")}>
+          Easy
         </Button>
-        <Button variant="dark" onClick={() => chooseImage("own")}>
-          My own image
+        <Button
+          variant="warning"
+          onClick={() => chooseDifficulty("intermediate")}
+        >
+          Intermediate
+        </Button>
+        <Button variant="warning" onClick={() => chooseDifficulty("hard")}>
+          Hard
         </Button>
       </div>
 
-      {showInput && (
-        <Form.Group controlId="formFile" className="mt-3">
-          <Form.Control
-            type="file"
-            onChange={(event) => {
-              console.log(event.target.files[0]);
-              setImage(event.target.files[0]);
-              setShowInput(false);
-            }}
-          />
-        </Form.Group>
+      {difficulty && (
+        <>
+          <div className="chooseImage">
+            <Button variant="success" onClick={() => chooseImage("default")}>
+              Default Image
+            </Button>
+            <Button variant="dark" onClick={() => chooseImage("own")}>
+              My own image
+            </Button>
+          </div>
+          {showInput && (
+            <Form.Group controlId="formFile">
+              <Form.Control
+                type="file"
+                onChange={(event) => {
+                  console.log(event.target.files[0]);
+                  setImage(event.target.files[0]);
+                  setShowInput(false);
+                }}
+              />
+            </Form.Group>
+          )}
+        </>
       )}
 
       {image && (
         <Board
           image={typeof image === "string" ? image : URL.createObjectURL(image)}
-          width={500}
-          height={500}
-          pieces={5}
+          width={difficulty * 100}
+          height={difficulty * 100}
+          pieces={difficulty}
         />
       )}
     </div>

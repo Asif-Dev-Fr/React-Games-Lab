f3a5da6cfebadf96962bbbe23bfd95dc14827091
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-12-16 10:26:34 +0100

    NavLink changed by Link

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import * as ProjectsList from "../assets/data/projectsList.json";
+import { Link } from "react-router-dom";
 
 const NavbarComponent = ({ isMobile }) => {
   const [projects] = useState(ProjectsList);
@@ -21,7 +22,7 @@ const NavbarComponent = ({ isMobile }) => {
           <Nav className="me-auto">
             {Object.entries(projects.list).map((project, key) => (
               <React.Fragment key={"project_" + key}>
-                <Nav.Link href={project[1].path}>{project[1].title}</Nav.Link>
+                <Link to={project[1].path}>{project[1].title}</Link>
               </React.Fragment>
             ))}
           </Nav>
